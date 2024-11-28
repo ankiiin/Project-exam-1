e6d57dbe9722babf2911f94da30dbb7515f7aa0d
ankiiin <annham49344@stud.noroff.no>
2024-11-28 13:11:08 +0100

    Making changes on the folder names, and the register button

--- JS/main.js
+++ JS/main.js
@@ -1,17 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-    const joinButton = document.querySelector(".join-button"); 
-    const joinPopup = document.getElementById("joinPopup"); 
-    const closeJoinPopup = document.querySelector("#joinPopup .close-button"); 
+    const registerButton = document.querySelector("#registerButton"); 
     const usernameDisplay = document.getElementById("username-display");
     const logoutButton = document.getElementById("logout-button");
     const editButton = document.getElementById("edit-button");
 
     const loggedInUser = localStorage.getItem("username");
 
-
     if (loggedInUser) {
         usernameDisplay.style.display = "none";  
+        registerButton.style.display = "none";  
         editButton.style.display = "inline-block"; 
         logoutButton.style.display = "inline-block";  
     }
@@ -20,36 +18,27 @@ document.addEventListener("DOMContentLoaded", () => {
         logoutButton.addEventListener("click", () => {
             localStorage.removeItem("username");
             localStorage.removeItem("accessToken");
-            usernameDisplay.textContent = "Join us!";
+            usernameDisplay.textContent = "Register";  
             usernameDisplay.style.display = "inline-block"; 
+            registerButton.style.display = "inline-block";  
             editButton.style.display = "none";
             logoutButton.style.display = "none"; 
             window.location.href = "/index.html";
         });
     }
 
-    if (joinButton && joinPopup && closeJoinPopup) {
-        joinButton.addEventListener("click", (event) => {
+    if (registerButton) {
+        registerButton.addEventListener("click", (event) => {
             event.preventDefault(); 
-            joinPopup.style.display = "flex"; 
-        });
-
-        closeJoinPopup.addEventListener("click", () => {
-            joinPopup.style.display = "none"; 
-        });
-
-        document.addEventListener("click", (event) => {
-            if (event.target === joinPopup) {
-                joinPopup.style.display = "none"; 
-            }
+            window.location.href = "../html/register.html"; 
         });
     }
 
     if (editButton) {
         editButton.addEventListener("click", () => {
-            if (loggedInUser) {
-                window.location.href = "../post/edit.html"; 
-            }
+            window.location.href = "../post/edit.html"; 
         });
     }
+
 });
+
