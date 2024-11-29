4ed08b80c3f90924525281d3e11d0b870c02d3d8
ankiiin <annham49344@stud.noroff.no>
2024-11-29 13:37:22 +0100

    Edits on blogpost page

--- js/blogpost.js
+++ js/blogpost.js
@@ -23,7 +23,15 @@ document.addEventListener("DOMContentLoaded", async () => {
 
             document.querySelector(".post-image img").src = data.media?.url || "https://via.placeholder.com/150";
             document.querySelector(".post-image img").alt = data.media?.alt || "Blog post image";
+
             document.querySelector(".post-content").innerHTML = data.body || "<p>No content available for this post.</p>";
+
+            if (data.author) {
+                document.querySelector(".post-author").textContent = `Author: ${data.author.name || "Unknown"}`;
+            } else {
+                document.querySelector(".post-author").textContent = "Author: Unknown";
+            }
+
         } else {
             document.querySelector(".post-content").innerHTML = "<p>Sorry, we couldn't load this post. Please try again later.</p>";
         }
